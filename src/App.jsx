1c1735151b8b7379011da3f64faaf9f71ed8dfd3
healrabbit <alphasorell@gmail.com>
2026-05-08 15:46:57 -0400

    trying again

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 
 const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 
-import '/App.css'
-import Homepage from '/index.jsx'
-import CaseStudies from '/casestudies.jsx'
-import Zack from '/zack.jsx'
-import Autumn from '/autumn.jsx'
-import Artifacts from '/artifacts.jsx'
-import LifeCycle from '/lifecycle.jsx'
-import Sensory from '/sensory.jsx'
-import Emotional from '/emotional.jsx'
-import Resistance from '/resistance.jsx'
-import Closing from '/closing.jsx'
-import References from '/references.jsx'
-import {Routes, Route } from "react-router-dom";
+import './App.css'
+import Homepage from './index.jsx'
+import CaseStudies from './casestudies.jsx'
+import Zack from './zack.jsx'
+import Autumn from './autumn.jsx'
+import Artifacts from './artifacts.jsx'
+import LifeCycle from './lifecycle.jsx'
+import Sensory from './sensory.jsx'
+import Emotional from './emotional.jsx'
+import Resistance from './resistance.jsx'
+import Closing from './closing.jsx'
+import References from './references.jsx'
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   return (
+    <BrowserRouter>
  <div>
       <Routes>
       <Route path="/" element={<Homepage/>} />
@@ -34,6 +35,7 @@ function App() {
 
      
  </div>
+ </BrowserRouter>
   )
 }
 
