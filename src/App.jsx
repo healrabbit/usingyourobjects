8effe2449446c7b0011c9abc484e01ee6804b2f2
healrabbit <alphasorell@gmail.com>
2026-05-08 10:29:34 -0400

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 
 const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 
-import './App.css'
-import Homepage from './index.jsx'
-import CaseStudies from './casestudies.jsx'
-import Zack from './zack.jsx'
-import Autumn from './autumn.jsx'
-import Artifacts from './artifacts.jsx'
-import LifeCycle from './lifecycle.jsx'
-import Sensory from './sensory.jsx'
-import Emotional from './emotional.jsx'
-import Resistance from './resistance.jsx'
-import Closing from './closing.jsx'
-import References from './references.jsx'
+import '/App.css'
+import Homepage from '/index.jsx'
+import CaseStudies from '/casestudies.jsx'
+import Zack from '/zack.jsx'
+import Autumn from '/autumn.jsx'
+import Artifacts from '/artifacts.jsx'
+import LifeCycle from '/lifecycle.jsx'
+import Sensory from '/sensory.jsx'
+import Emotional from '/emotional.jsx'
+import Resistance from '/resistance.jsx'
+import Closing from '/closing.jsx'
+import References from '/references.jsx'
 import {Routes, Route } from "react-router-dom";
 function App() {
 
